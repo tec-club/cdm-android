c9c8368115ebb0fc1405a3ca2bf7b10f9935f630
Alex Wendland <alex.wendland1@gmail.com>
2013-02-24 09:20:56 -0800

    Setup for new screens/features

---
+++ CDM/src/com/ampelement/cdm/fragments/EBCheckerFragment.java
@@ -0,0 +1,210 @@
+package com.ampelement.cdm.fragments;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CookieStore;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import com.ampelement.cdm.R;
+import com.ampelement.cdm.objects.Preferences;
+import com.ampelement.cdm.utils.SchoolLoopAPI;
+
+public class EBCheckerFragment extends SherlockFragment {
+
+	private RelativeLayout schoolLoopScreen;
+
+	boolean newCredentials = false;
+
+	public WebView webView;
+	SharedPreferences sharedPreferences;
+
+	public static final String TAG = "SchoolLoopFragment";
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		// Loading Screen Handler
+		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+		// Get Screens
+		schoolLoopScreen = (RelativeLayout) inflater.inflate(R.layout.school_loop_screen, container, false);
+		final LinearLayout loginScreen = (LinearLayout) schoolLoopScreen.findViewById(R.id.school_loop_inputs);
+		final RelativeLayout loadingScreen = (RelativeLayout) schoolLoopScreen.findViewById(R.id.school_loop_loading);
+		// Attempt to retrieve stored credentials
+		String username = sharedPreferences.getString(Preferences.SCHOOL_LOOP_USERNAME, "");
+		String password = sharedPreferences.getString(Preferences.SCHOOL_LOOP_PASSWORD, "");
+		// If credentials don't exist then show login screen
+		webView = new WebView(getActivity().getApplicationContext());
+		((LinearLayout) schoolLoopScreen.findViewById(R.id.school_loop_webview_screen)).addView(webView);
+		if (username.matches("") || password.matches("")) {
+			// Show Login Screen
+			loginScreen.setVisibility(View.VISIBLE);
+			final EditText usernameInput = (EditText) schoolLoopScreen.findViewById(R.id.school_loop_username);
+			final EditText passwordInput = (EditText) schoolLoopScreen.findViewById(R.id.school_loop_password);
+			final Button submitButton = (Button) schoolLoopScreen.findViewById(R.id.school_loop_submit);
+			submitButton.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					// Get Login Credentials
+					String[] credentialStringArray = new String[2];
+					credentialStringArray[0] = usernameInput.getEditableText().toString();
+					credentialStringArray[1] = passwordInput.getEditableText().toString();
+					// Check for blank credentials
+					if (credentialStringArray[0].matches("") || credentialStringArray[1].matches("")) {
+						TextView errorText = (TextView) schoolLoopScreen.findViewById(R.id.school_loop_error);
+						errorText.setVisibility(View.VISIBLE);
+						if (credentialStringArray[0].matches("") && credentialStringArray[1].matches("")) {
+							errorText.setText("Username and Password are Blank");
+						} else {
+							errorText.setText(((credentialStringArray[0].matches("")) ? "Username" : "Password") + "is Blank");
+						}
+					} else {
+						// Attempt Login
+						loginScreen.setVisibility(View.GONE);
+						loadingScreen.setVisibility(View.VISIBLE);
+						newCredentials = true;
+						new SchoolLoopLoginTask().execute(credentialStringArray);
+					}
+				}
+			});
+		} else {
+			// Load School Loop Mobile page
+			loadingScreen.setVisibility(View.VISIBLE);
+			String[] credentialStringArray = new String[2];
+			credentialStringArray[0] = username;
+			credentialStringArray[1] = password;
+			new SchoolLoopLoginTask().execute(credentialStringArray);
+		}
+		return schoolLoopScreen;
+	}
+
+	private class SchoolLoopLoginTask extends AsyncTask<String, Void, Boolean> {
+
+		@Override
+		protected Boolean doInBackground(String... cred) {
+			long lastActiveTime = sharedPreferences.getLong(Preferences.SCHOOL_LOOP_TIME, 0);
+			SharedPreferences.Editor sharedPrefEditor = sharedPreferences.edit();
+			if (lastActiveTime != 0 && (System.currentTimeMillis() - lastActiveTime) < 900000) { // Logged
+																									// in
+																									// still
+				CookieSyncManager syncManager = CookieSyncManager.createInstance(webView.getContext());
+				CookieManager cookieManager = CookieManager.getInstance();
+				cookieManager.setCookie("http://cdm.schoolloop.com/", "JSESSIONID=" + sharedPreferences.getString(Preferences.SCHOOL_LOOP_JSESSIONID, ""));
+				cookieManager.setCookie("http://cdm.schoolloop.com/", "slid=" + sharedPreferences.getString(Preferences.SCHOOL_LOOP_SLID, ""));
+				CookieSyncManager.getInstance().sync();
+				loadWebView();
+				return true;
+			} else { // Not logged in
+				if (cred.length == 2) {
+					try {
+						CookieStore loginCookieStore = SchoolLoopAPI.loginToSchoolloop(new DefaultHttpClient(), cred[0], cred[1], true);
+						if (loginCookieStore != null) {
+							CookieSyncManager syncManager = CookieSyncManager.createInstance(webView.getContext());
+							CookieManager cookieManager = CookieManager.getInstance();
+							for (Cookie cookie : loginCookieStore.getCookies()) {
+								cookieManager.setCookie((cookie.isSecure() ? "https" : "http") + "://" + cookie.getDomain() + cookie.getPath(), cookie.getName() + "=" + cookie.getValue());
+								if (cookie.getName().matches("slid")) {
+									sharedPrefEditor.putString(Preferences.SCHOOL_LOOP_SLID, cookie.getValue());
+									sharedPrefEditor.commit();
+								} else if (cookie.getName().matches("JSESSIONID")) {
+									sharedPrefEditor.putString(Preferences.SCHOOL_LOOP_JSESSIONID, cookie.getValue());
+									sharedPrefEditor.commit();
+								}
+							}
+							CookieSyncManager.getInstance().sync();
+							if (newCredentials) {
+								sharedPrefEditor.putString(Preferences.SCHOOL_LOOP_USERNAME, cred[0]);
+								sharedPrefEditor.putString(Preferences.SCHOOL_LOOP_PASSWORD, cred[1]);
+								sharedPrefEditor.commit();
+							}
+							loadWebView();
+							sharedPrefEditor.putLong(Preferences.SCHOOL_LOOP_TIME, System.currentTimeMillis()).commit();
+							return true;
+						} else {
+							return false;
+						}
+					} catch (ClientProtocolException e) {
+						return null;
+					} catch (IOException e) {
+						return null;
+					}
+				} else {
+					return null;
+				}
+			}
+		}
+
+		private void loadWebView() {
+			webView.getSettings().setJavaScriptEnabled(true);
+			webView.setWebViewClient(new SchoolLoopWebViewClient());
+			webView.loadUrl(SchoolLoopAPI.BASE_URL_SECURE + "/mobile/index");
+		}
+
+		@Override
+		protected void onPostExecute(Boolean success) {
+			if (success != null) {
+				if (success) {
+					((LinearLayout) schoolLoopScreen.findViewById(R.id.school_loop_webview_screen)).setVisibility(View.VISIBLE);
+				} else {
+					loginScreenWithErrorMessage("Bad Username or Password");
+				}
+			} else {
+				loginScreenWithErrorMessage("Error Logging In");
+			}
+		}
+	}
+
+	private void loginScreenWithErrorMessage(String errorString) {
+		((RelativeLayout) schoolLoopScreen.findViewById(R.id.school_loop_loading)).setVisibility(View.GONE);
+		LinearLayout loginScreen = (LinearLayout) schoolLoopScreen.findViewById(R.id.school_loop_inputs);
+		loginScreen.setVisibility(View.VISIBLE);
+		TextView errorText = (TextView) schoolLoopScreen.findViewById(R.id.school_loop_error);
+		errorText.setVisibility(View.VISIBLE);
+		errorText.setText(errorString);
+	}
+
+	private class SchoolLoopWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			if (url.contains("portal/logout")) {
+				SharedPreferences.Editor sharedPrefEditor = sharedPreferences.edit();
+				sharedPrefEditor.putString(Preferences.SCHOOL_LOOP_USERNAME, "");
+				sharedPrefEditor.putString(Preferences.SCHOOL_LOOP_PASSWORD, "");
+				sharedPrefEditor.putLong(Preferences.SCHOOL_LOOP_TIME, 0);
+				sharedPrefEditor.commit();
+				((RelativeLayout) schoolLoopScreen.findViewById(R.id.school_loop_loading)).setVisibility(View.GONE);
+				((LinearLayout) schoolLoopScreen.findViewById(R.id.school_loop_webview_screen)).setVisibility(View.GONE);
+				LinearLayout loginScreen = (LinearLayout) schoolLoopScreen.findViewById(R.id.school_loop_inputs);
+				loginScreen.startAnimation(AnimationUtils.loadAnimation(getActivity().getApplicationContext(), android.R.anim.fade_in));
+				loginScreen.setVisibility(View.VISIBLE);
+			} else {
+				view.loadUrl(url);
+				sharedPreferences.edit().putLong(Preferences.SCHOOL_LOOP_TIME, System.currentTimeMillis()).commit();
+			}
+			return true;
+		}
+	}
+
+}
---
+++ CDM/src/com/ampelement/cdm/fragments/WebViewFragment.java
@@ -0,0 +1,138 @@
+package com.ampelement.cdm.fragments;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import com.ampelement.cdm.R;
+
+public class WebViewFragment extends SherlockFragment {
+
+	private static final String BASE_URL = "http://www.ampelement.com/cdm";
+
+	private GridView mInfoGridView;
+	private LinearLayout mWebViewLinearLayout;
+	private ScrollView mInfoScrollView;
+
+	public WebView mWebView;
+
+	public static final String TAG = "InfoListFragment";
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+		View infoScreen = inflater.inflate(R.layout.info_screen, container, false);
+		mInfoGridView = (GridView) infoScreen.findViewById(R.id.info_screen_gridView);
+		mWebViewLinearLayout = (LinearLayout) infoScreen.findViewById(R.id.info_screen_webView);
+		mInfoScrollView = (ScrollView) infoScreen.findViewById(R.id.info_screen_scrollView);
+
+		mWebView = new WebView(getActivity());
+		mWebView.setWebViewClient(new InfoWebViewClient());
+		mWebView.getSettings().setBuiltInZoomControls(true);
+		mWebView.getSettings().setUseWideViewPort(true);
+		mWebView.getSettings().setLoadWithOverviewMode(true);
+		mWebViewLinearLayout.addView(mWebView);
+
+		mInfoGridView.setAdapter(new InfoAdapter(getActivity().getApplicationContext()));
+
+		return infoScreen;
+	}
+
+	private class InfoItem {
+		String name;
+		int thumbnailRes;
+		String url;
+
+		public InfoItem(String name, int thumbnailRes, String url) {
+			this.name = name;
+			this.thumbnailRes = thumbnailRes;
+			this.url = url;
+		}
+	}
+
+	public boolean showInfoItemSelecter() {
+		if (mWebViewLinearLayout.isShown()) {
+			mInfoScrollView.setVisibility(View.VISIBLE);
+			mWebViewLinearLayout.setVisibility(View.GONE);
+			return true;
+		} else
+			return false;
+	}
+
+	private class InfoAdapter extends BaseAdapter {
+		ArrayList<InfoItem> infoList;
+		LayoutInflater layoutInflater;
+
+		public InfoAdapter(Context context) {
+			layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			infoList = new ArrayList<InfoItem>();
+			infoList.add(new InfoItem("Handbook", R.drawable.info_handbook, BASE_URL + "/handbook"));
+			infoList.add(new InfoItem("Bell Schedule", R.drawable.info_bell_schedule, BASE_URL + "/bell_schedule"));
+			infoList.add(new InfoItem("Year Schedule", R.drawable.info_bell_schedule, "http://cdm.schoolloop.com/file/1211914146706/1229223566913/7406136305481272074.pdf"));
+		}
+
+		@Override
+		public int getCount() {
+			return infoList.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// Get InfoItem information
+			final String name = infoList.get(position).name;
+			final int thumbnailRes = infoList.get(position).thumbnailRes;
+			final String url = infoList.get(position).url;
+			// Create view elements
+			LinearLayout itemLayout = (LinearLayout) layoutInflater.inflate(R.layout.info_item_view, null);
+			ImageView itemThumbnail = (ImageView) itemLayout.findViewById(R.id.info_item_imageView);
+			TextView itemTitle = (TextView) itemLayout.findViewById(R.id.info_item_textView);
+			// Set view items to corresponding InfoItem information
+			itemThumbnail.setImageResource(thumbnailRes);
+			itemTitle.setText(name);
+			// Set webView to load page on InfoItem click
+			itemThumbnail.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					mWebView.loadUrl(url);
+					mInfoScrollView.setVisibility(View.GONE);
+					mWebViewLinearLayout.setVisibility(View.VISIBLE);
+				}
+			});
+
+			return itemLayout;
+		}
+	}
+
+	private class InfoWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
+	}
+
+}
