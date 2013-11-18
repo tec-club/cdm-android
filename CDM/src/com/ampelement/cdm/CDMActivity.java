c3e5c6e54d9c20b946a9b72e54116a40f2113448
Alex Wendland <me@alexwendland.com>
2013-11-17 19:45:49 -0800

    Put backPress code in a try/catch. Setup any exceptions to have their stackTrace emailed

--- CDM/src/com/ampelement/cdm/CDMActivity.java
+++ CDM/src/com/ampelement/cdm/CDMActivity.java
@@ -1,11 +1,15 @@
 package com.ampelement.cdm;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.graphics.drawable.TransitionDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.FragmentManager;
@@ -39,10 +43,10 @@ public class CDMActivity extends SherlockFragmentActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
+
 		getSupportActionBar().setLogo(R.drawable.crown);
 		getSupportActionBar().setDisplayUseLogoEnabled(true);
-//		getSupportActionBar().setTitle("Corona del Mar");
+		// getSupportActionBar().setTitle("Corona del Mar");
 		getSupportActionBar().setTitle(Html.fromHtml("<b><font color='#ffffff'>Corona del Mar</font></b>"));
 
 		mViewPager = (ViewPager) findViewById(R.id.main_viewpager);
@@ -62,9 +66,9 @@ public class CDMActivity extends SherlockFragmentActivity {
 
 		mViewPager.setAdapter(mCDMTabsAdapter);
 		mPagerSlidingTabs.setViewPager(mViewPager);
-		
+
 		mPagerSlidingTabs.setOnPageChangeListener(new DefaultViewPagerOnChangeListener() {
-			
+
 			@Override
 			public void onPageScrollStateChanged(int position) {
 				changeActionBarColor(mCDMTabsAdapter.getCurrentItem().color);
@@ -75,37 +79,37 @@ public class CDMActivity extends SherlockFragmentActivity {
 
 		mViewPager.setOffscreenPageLimit(2);
 	}
-	
+
 	Drawable oldActionBar = null;
-	
+
 	public void changeActionBarColor(int color) {
 		mPagerSlidingTabs.setIndicatorColor(mCDMTabsAdapter.getCurrentItem().color);
-		
+
 		Drawable colorDrawable = new ColorDrawable(color);
 		Drawable bottomDrawable = getResources().getDrawable(R.drawable.actionbar_bottom);
 		LayerDrawable ld = new LayerDrawable(new Drawable[] { colorDrawable, bottomDrawable });
-//		getSupportActionBar().setBackgroundDrawable(ld);
-//		getSupportActionBar().setDisplayShowTitleEnabled(false);
-//		getSupportActionBar().setDisplayShowTitleEnabled(true);
-		
+		// getSupportActionBar().setBackgroundDrawable(ld);
+		// getSupportActionBar().setDisplayShowTitleEnabled(false);
+		// getSupportActionBar().setDisplayShowTitleEnabled(true);
+
 		if (oldActionBar != null) {
 			TransitionDrawable td = new TransitionDrawable(new Drawable[] { oldActionBar, ld });
 			getSupportActionBar().setBackgroundDrawable(td);
-//			td.setCallback(drawableCallback);
+			// td.setCallback(drawableCallback);
 			td.startTransition(200);
 			oldActionBar = td;
 		} else {
 			getSupportActionBar().setBackgroundDrawable(ld);
 			oldActionBar = ld;
 		}
-		
+
 		getSupportActionBar().setDisplayShowTitleEnabled(false);
 		getSupportActionBar().setDisplayShowTitleEnabled(true);
-		
+
 	}
-	
+
 	private Handler handler = new Handler();
-	
+
 	private Drawable.Callback drawableCallback = new Drawable.Callback() {
 		@Override
 		public void invalidateDrawable(Drawable who) {
@@ -125,20 +129,30 @@ public class CDMActivity extends SherlockFragmentActivity {
 
 	@Override
 	public void onBackPressed() {
-		SherlockFragment fragment = mCDMTabsAdapter.getCurrentFragment();
-		if (fragment != null) {
-			if (fragment != null && fragment instanceof SchoolLoopFragment) {
-				SchoolLoopFragment schoolLoopFragment = (SchoolLoopFragment) fragment;
-				if (schoolLoopFragment.webView.canGoBack()) {
-					schoolLoopFragment.webView.goBack();
+		try {
+			SherlockFragment fragment = mCDMTabsAdapter.getCurrentFragment();
+			if (fragment != null) {
+				if (fragment != null && fragment instanceof SchoolLoopFragment) {
+					SchoolLoopFragment schoolLoopFragment = (SchoolLoopFragment) fragment;
+					if (schoolLoopFragment.webView.canGoBack()) {
+						schoolLoopFragment.webView.goBack();
+					} else {
+						super.onBackPressed();
+					}
 				} else {
 					super.onBackPressed();
 				}
-			} else {
+			} else
 				super.onBackPressed();
-			}
-		} else
-			super.onBackPressed();
+		} catch (Exception e) {
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			e.printStackTrace(pw);
+			Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "me@alexwendland.com", null));
+			emailIntent.putExtra(Intent.EXTRA_SUBJECT, e.getMessage());
+			emailIntent.putExtra(Intent.EXTRA_TEXT, sw.toString());
+			startActivity(Intent.createChooser(emailIntent, "Send email..."));
+		}
 	}
 
 	private static class TitledFragment {
