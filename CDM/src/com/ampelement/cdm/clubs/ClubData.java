2584904377c14b94ed0b5d06f8e20d8dbac72504
Alex Wendland <me@alexwendland.com>
2013-12-03 17:05:03 -0800

    Add advisor and president fields to club data

--- CDM/src/com/ampelement/cdm/clubs/ClubData.java
+++ CDM/src/com/ampelement/cdm/clubs/ClubData.java
@@ -5,10 +5,11 @@ import android.net.Uri;
 public class ClubData {
 
 	public String name;
-	public String logoURL;
+	public String logoURL;// = "http://thecatapi.com/api/images/get?format=src&type=jpg&ran=" + Integer.toHexString((int) (Math.random() * 10000));
 	public String description;
 	public String[] meetingTimes;
-	
+	public String president;
+	public String advisor;
 
 	public Uri getLogoUrl() {
 		return Uri.parse(logoURL);
