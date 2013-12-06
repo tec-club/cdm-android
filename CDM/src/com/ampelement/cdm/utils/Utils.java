03a67de3e79658182eb541e8bf1370091b1f6374
Alex Wendland <me@alexwendland.com>
2013-12-05 18:29:57 -0800

    Add convenience URL to String method

--- CDM/src/com/ampelement/cdm/utils/Utils.java
+++ CDM/src/com/ampelement/cdm/utils/Utils.java
@@ -1,5 +1,11 @@
 package com.ampelement.cdm.utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class Utils {
 
 	/**
@@ -32,6 +38,43 @@ public class Utils {
 		}
 		sb.delete(sb.length() - c.length(), sb.length());
 		return sb.toString();
+	}
+
+	/**
+	 * Open an InputStream of the supplied URL and read the response into a
+	 * String object.
+	 * 
+	 * @param url
+	 *            URL to fetch
+	 * @return String response from the URL
+	 */
+	public static String getURL(URL url) {
+		StringBuilder response = new StringBuilder();
+
+		InputStream is = null;
+		BufferedReader br = null;
+		try {
+			is = url.openStream();
+			br = new BufferedReader(new InputStreamReader(is));
+
+			String line;
+			while ((line = br.readLine()) != null)
+				response.append(line);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+				if (is != null)
+					is.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return response.toString();
 	}
 
 }
