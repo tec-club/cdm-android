aec08144b9eea8691117ec03646939c1f1cb442e
Alex Wendland <me@alexwendland.com>
2013-12-05 16:34:25 -0800

    Optimize InfoFragment: removed unused views/sub-classes, remove deprecated showUserHint method, implement click handler on the ListView instead of the individual row views.

--- CDM/src/com/ampelement/cdm/infoscreen/InfoListFragment.java
+++ CDM/src/com/ampelement/cdm/infoscreen/InfoListFragment.java
@@ -6,12 +6,9 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.view.animation.AnimationUtils;
-import android.view.animation.LayoutAnimationController;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -24,9 +21,13 @@ import com.ampelement.cdm.utils.android.TitledSherlockFragment;
 public class InfoListFragment extends TitledSherlockFragment {
 
 	public static final String TAG = "InfoListFragment";
+	
 	private static final String BASE_URL = "http://www.ampelement.com/cdm";
+	
 	private ListView mInfoListView;
 	
+	private InfoAdapter mInfoAdapter;
+
 	@Override
 	public String getTitle() {
 		return "Info / Handbook";
@@ -37,26 +38,20 @@ public class InfoListFragment extends TitledSherlockFragment {
 
 		View infoScreen = inflater.inflate(R.layout.info_screen, container, false);
 		mInfoListView = (ListView) infoScreen.findViewById(R.id.info_screen_gridView);
+		
+		mInfoAdapter = new InfoAdapter(getSherlockActivity());
+		mInfoListView.setAdapter(mInfoAdapter);
 
-		mInfoListView.setAdapter(new InfoAdapter(getSherlockActivity()));
-		mInfoListView.setVisibility(View.INVISIBLE);
-		mInfoListView.setLayoutAnimation(new LayoutAnimationController(AnimationUtils.loadAnimation(getSherlockActivity(), R.anim.slide_up), .5f));
-		return infoScreen;
-	}
+		mInfoListView.setOnItemClickListener(new OnItemClickListener() {
 
-	boolean shownAnimation = false;
-
-	@Override
-	public void setUserVisibleHint(boolean isVisibleToUser) {
-		super.setUserVisibleHint(isVisibleToUser);
-		if (isVisibleToUser) {
-			if (!shownAnimation) {
-				mInfoListView.setVisibility(View.VISIBLE);
-				shownAnimation = true;
+			@Override
+			public void onItemClick(AdapterView<?> listView, View rowView, int position, long itemID) {
+				InfoItem item = mInfoAdapter.getItem(position);
+				WebViewDialogFragment.newInstance(item.url, false, item.initialScaleFull).show(getSherlockActivity().getSupportFragmentManager(), item.name);
 			}
-		} else {
-		}
+		});
 
+		return infoScreen;
 	}
 
 	static class ViewHolder {
@@ -106,12 +101,14 @@ public class InfoListFragment extends TitledSherlockFragment {
 					BASE_URL + "/bell_schedule",
 					"The school bell schedule. Contains time information for Late Starts, Minimum Days, Regular Days, Final Exams, Assmebly/Rally Schedule, CST Exams and Senior Projects.",
 					false));
-			/*infoList.add(new InfoItem(
-					"Year Schedule",
-					R.drawable.info_bell_schedule,
-					"http://cdm.schoolloop.com/file/1211914146706/1229223566913/7406136305481272074.pdf",
-					"The yearly schedule. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum malesuada purus quis lacus adipiscing elementum.",
-					false));*/
+			/*
+			 * infoList.add(new InfoItem( "Year Schedule",
+			 * R.drawable.info_bell_schedule,
+			 * "http://cdm.schoolloop.com/file/1211914146706/1229223566913/7406136305481272074.pdf"
+			 * ,
+			 * "The yearly schedule. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum malesuada purus quis lacus adipiscing elementum."
+			 * , false));
+			 */
 		}
 
 		@Override
@@ -120,7 +117,7 @@ public class InfoListFragment extends TitledSherlockFragment {
 		}
 
 		@Override
-		public Object getItem(int position) {
+		public InfoItem getItem(int position) {
 			return infoList.get(position);
 		}
 
@@ -163,23 +160,8 @@ public class InfoListFragment extends TitledSherlockFragment {
 			holder.thumbnail.setImageResource(thumbnailRes);
 			holder.title.setText(name);
 			holder.description.setText(description);
-			// Set webView to load page on InfoItem click
-			convertView.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					WebViewDialogFragment.newInstance(url, false, initialScale).show(getSherlockActivity().getSupportFragmentManager(), name);
-				}
-			});
 
 			return convertView;
-		}
-	}
-
-	private class InfoWebViewClient extends WebViewClient {
-		@Override
-		public boolean shouldOverrideUrlLoading(WebView view, String url) {
-			view.loadUrl(url);
-			return true;
 		}
 	}
 
