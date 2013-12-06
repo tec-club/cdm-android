95e7b01aec77a76329598518dcea22b53229efe8
Alex Wendland <me@alexwendland.com>
2013-12-05 16:20:03 -0800

    Remove Roboto-Light textview and create a Roboto-Regular textview

--- CDM/src/com/ampelement/cdm/widgets/TextViewRR.java
+++ CDM/src/com/ampelement/cdm/widgets/TextViewRR.java
@@ -5,26 +5,28 @@ import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
-public class TextViewRL extends TextView {
+public class TextViewRR extends TextView {
+	
+	private String mFontPath = "fonts/Roboto-Regular.ttf";
 
-    public TextViewRL(Context context, AttributeSet attrs, int defStyle) {
+    public TextViewRR(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         init();
     }
 
-    public TextViewRL(Context context, AttributeSet attrs) {
+    public TextViewRR(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
     }
 
-    public TextViewRL(Context context) {
+    public TextViewRR(Context context) {
         super(context);
         init();
     }
 
     private void init() {
         if (!isInEditMode()) {
-            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/Roboto-Light.ttf");
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), mFontPath);
             setTypeface(tf);
         }
     }
