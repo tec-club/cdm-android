8345b0646ffdf8ae375ea34e3b81f58acefb87a0
Alex Wendland <me@alexwendland.com>
2014-05-23 20:07:14 -0700

    Migration to fresh Android Studio project

--- app/src/com/ampelement/cdm/utils/android/ExtendedSherlockFragment.java
+++
@@ -1,9 +0,0 @@
-package com.ampelement.cdm.utils.android;
-
-import com.actionbarsherlock.app.SherlockFragment;
-
-public abstract class ExtendedSherlockFragment extends SherlockFragment {
-	
-	public abstract String getFragmentTag();
-	
-}
---
+++ app/src/main/java/com/ampelement/cdm/utils/android/ExtendedSherlockFragment.java
@@ -0,0 +1,9 @@
+package com.ampelement.cdm.utils.android;
+
+import android.app.Fragment;
+
+public abstract class ExtendedSherlockFragment extends Fragment {
+	
+	public abstract String getFragmentTag();
+	
+}
